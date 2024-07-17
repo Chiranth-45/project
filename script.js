283404aa5215e95cd8c5424b5c350da5c5b8a90f
Chiranth-45 <150377345+Chiranth-45@users.noreply.github.com>
2024-07-17 14:24:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,4 +2,5 @@ console.log("hello");
 let btn=document.querySelector("button");
 btn.addEventListener("click",function(){
     // add some event to the button
+    //callback function can be return the function
 });
