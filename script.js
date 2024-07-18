cfee220af531a71458b4076d0b9292ff15d32528
Chiranth-45 <150377345+Chiranth-45@users.noreply.github.com>
2024-07-18 20:41:42 +0530

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-console.log("hello");
-let btn=document.querySelector("button");
-btn.addEventListener("click",function(){
-    // add some event to the button
-    //callback function can be return the function
-});
